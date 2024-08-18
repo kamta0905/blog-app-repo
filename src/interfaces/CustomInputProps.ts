6b8ca7b264b972b2ab44de81a243253353224a26
shashikant yadav <shashikant.sy126@gmail.com>
2024-08-18 21:01:25 +0530

    remove

--- src/interfaces/CustomInputProps.ts
+++
@@ -1,8 +0,0 @@
-import { TextFieldProps, InputProps as MuiInputProps } from "@mui/material";
-import { ReactNode } from "react";
-export interface CustomInputProps extends Omit<TextFieldProps, "inputProps"> {
-  inputProps?: Partial<MuiInputProps> | any; // Correctly type inputProps
-  startAdornment?: ReactNode;
-  endAdornment?: ReactNode;
-  shrink?: boolean;
-}
--- src/interfaces/PrimaryButtonProps.ts
+++
@@ -1,9 +0,0 @@
-import { ButtonProps } from "@mui/material/Button";
-import React from "react";
-
-export interface PrimaryButtonProps extends ButtonProps {
-  btnSize?: string;
-  btnColor?: "default" | "inherit" | "primary" | "secondary";
-  iconLeft?: React.ReactNode;
-  label?: React.ReactNode;
-}
